3a91441244a774dd57f9cc4c4c8e22cc661d8435
Rufaro Bernards <rufarobernards@gmail.com>
2025-09-15 05:31:04 +0000

    The images are just playing fast then dissappear, they are supposed to b

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -79,14 +79,25 @@ export default {
           },
         },
         'fade-in': {
-          from: { opacity: '0.5' },
+          from: { opacity: '0' },
           to: { opacity: '1' },
         },
+        'fade-in-up': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(10px)'
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)'
+          },
+        }
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'fade-in': 'fade-in 1s ease-in-out',
+        'fade-in': 'fade-in 1s ease-in-out forwards',
+        'fade-in-up': 'fade-in-up 0.5s ease-out forwards',
       },
       aspectRatio: {
         '4/3': '4 / 3',
