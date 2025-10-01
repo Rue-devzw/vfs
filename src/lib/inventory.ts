73681afd8e231dab14f7908286ae83821fd63f32
rufarobernards <rufarobernards@gmail.com>
2025-10-01 05:28:21 +0000

    inventory

---
+++ src/lib/inventory.ts
@@ -0,0 +1,364 @@
+
+export const inventory = `
+Category: 001 - BABY
+
+BABC: Baby & Me Cream
+
+EPX: Epimax Cream 400g
+
+HUGD: Huggies Diapers - Pack
+
+HUGW: Huggies Wipes
+
+JOH: Johnson Jelly 325ml
+
+JOHA: Johnson Aqueous 350ml
+
+JOHN: Johnson Baby soap 175g
+
+MASTW: Masters Wipes
+
+NAN: Nan 1;2;3&4
+
+PAMP: Pampers Masters Pack
+
+PAMPS: Pampers Masters Single
+
+PRED: Predo Wipes
+
+PURA: Purity Aqueous
+
+SOFT: Soft Care Pampers
+
+SUNS: Sunlight soap
+
+SUPRDY: Superdry - Pamper
+
+WIP: Wipes
+
+WIPJOHN: Johnson Wipes
+
+Category: 004 - CLEANING PRODUCTS
+
+ALH: Aloha Washing Pwd 2kg
+
+ALO: Aloha Washing Pwd 1kg
+
+ALOD: Aloha Dishwasher
+
+ALOH: Aloha Washing Pwdr 500g
+
+ARP: Arpic Cleaner 750ml
+
+BIG: BigBen bar
+
+BOM: Boom Paste
+
+BOMFC: Boom Force Cream 750ml
+
+BOMM: Boom 150g
+
+BMW: Boom - 1KG
+
+BOO: BoomDish washer 750ml
+
+BOOMB: Boom Bleach 750ml
+
+BOOMF: Boom Force Scouring
+
+BOOMM: Boom W/P 500g
+
+COB: Cobra 350ml
+
+COM: Comfort 1L
+
+DISW: Dishwasher Seawave
+
+DOMES: Domestos local
+
+DOMS: Domestos
+
+GK: Gik 750ml
+
+GREE: Green Bar Perfection
+
+GRN: Green Bar
+
+HAN: Handy Andy
+
+HARP: Harpic
+
+JK: Jik 750ml
+
+KIW: Kiwi Polish 50ml
+
+KNCK: Knock Dishwasher 750ml
+
+KNO: Knockout Can
+
+KNOA: Knockout All Purpose Cleaner 500ml
+
+KNOC: Knock out Satchet
+
+KNOCT: Knockout Dishwasher 2L
+
+KNOCK: Knockout Bleach
+
+KWI: Kiwi 100ml
+
+MAQ: Maq 1kg
+
+MEQFLR: Boom 2kg
+
+MQ: Maq 2kg
+
+NUGET: Nugget - 100ml
+
+OMO: Omo Powder 2kg
+
+PINE: Pinegel 500ml
+
+PNE: Pinegel 1L
+
+SEAW: DishWasher Seawave 2L
+
+SNL: Sunlight Liquid
+
+STASFT: Star Soft - 2litre
+
+STS: StarSoft Refill mix
+
+SUNL: Sunlight 1kg
+
+SUNLT: SunLight Pwd 2kg
+
+VIM: Xtra Vim
+
+VM: Vim Can
+
+XTRA: XTRA Washing Powder
+
+Category: 005 - COSMETICS
+
+ADDSPRY: Addidas Spray
+
+AXP: Axe Perfumer
+
+BAB: Baby Crez 500ml
+
+BABL: Baby line 500ml
+
+BCS: Black Chick Small 125ml
+
+BEV: BeverlyHills Roll on
+
+BEVP: Beverly Hills Perfume
+
+BLC: BlackChic Big 250ml
+
+BOD: Bodi doc
+
+BODD: Bodi doc Tissue Oil
+
+BOMO: Boom Fabric Conditioner 2ltr
+
+CBL: Clere Beautiful Cream 400ml
+
+CFM: Clere For Men 450ml
+
+CLG: Clere Glycerine 100ml
+
+CLM: Clere Men lotion 400ml
+
+CLR: Clere Glyco Glycerine 100ml
+
+CLRE: Clere Ladies Cream
+
+COSMO: Cosmo Foam Bath
+
+DAR: Dark& Lovely
+
+DAW: Dawn
+
+DAWN: Dawn Cream
+
+DAX31: Dax 3 in 1 375mls
+
+DAXG: Dax Hair Grower 250ml
+
+DAXH: Dax Hair Grower 125mls
+
+DAXI: Dax Freez Big 250ml
+
+DOVE: Dove Lotion
+
+DXA: Dax Freez Small 100ml
+
+DXB: Dax Big 3 n 1
+
+ENG: English Blazer
+
+EWP: EasyWaves Pack
+
+EXCLA: Exclamation Perfume
+
+GML: Gentel Magic lotion
+
+GML: Gentle Magic Cream
+
+GMS: Gentel Magic Serum
+
+GNT: Gentel Magic Soap
+
+GNTO: Gentle Magic Oil 50ml
+
+HAR: Hair Remover Cosmo 40g
+
+HOIT: Hoit Toity 90mls
+
+HOITY: Hoity Toity Lotion
+
+IGM: Ingrams Herbal
+
+IGMS: Ingrams Senstive 450ml
+
+IMP: Ingrams Moisture Plus 450ml
+
+ING: Ingrams men 450ml
+
+INGM: Ingrams Aloe 450ml
+
+INGRO: Ingrams Original 450mls
+
+INR: Ingrams tissue oi l450ml
+
+ISB: Inecto Super Black75g
+
+ISOP: Isoplus Edge Control
+
+LACE: Lace Lotion
+
+MOUS: Mousse
+
+NIV: Nivea Cream/Lotion 400ml
+
+NIVA: Nivea Radiant Cream 400ml
+
+NIVES: Nivea Spray
+
+NVIA: Nivea Q10
+
+OHS: Oh So Heavenly Lotion 720ml
+
+OHSHV: Oh So Heavenly - 2L
+
+OHSL: Oh So Heavenly Lotion 1L
+
+OHSO: Oh SO Heavenly Cream 450ml
+
+OHSOQ: Oh So heavenly Q10
+
+OLV: Olive Oil Spry 250ml
+
+PLA: PlayBoy Roll on
+
+PLAY: PlayBoy Perfume
+
+RLS: Roll On Shield
+
+ROLL: Roll On Addidas 50ml
+
+ROLNIV: Roll on Nivea - 50ml
+
+ROP: Roll On Power House 50ml
+
+SHOW: Shower To Shower
+
+SHWR: Shower to Shower Perfume
+
+SKA: Skala 200g
+
+SKAL: Skala hairFood
+
+TOPS: Top Society
+
+VAS: Vaseline 450ml
+
+VASE: Vaseline 250ml
+
+VASEL: v
+
+VASELN: Vaseline lips
+
+VASL: Vaseline 50ml
+
+VASLN: Vaseline Cream
+
+VSNL: Vaseline Lotion
+
+VESTGLC: Vestline Garlic - 200g
+
+VSN: Vaseline - 100ml
+
+Category: 019 - TOILETRIES
+
+ANG: Angel Care Tissue
+
+AQFRSH: Aquafresh - 100ml
+
+BABYS: Baby Soft 18s
+
+CLO: Close Up
+
+COL: Colgate 100ml
+
+DELLP: Utra Pads
+
+DETO: Dettol Liquid
+
+DETT: Dettol soap
+
+GEI: Geisha
+
+HIL: HiLife Soap
+
+HYG: hygienix Soap 175g
+
+JAD: Jade Soap
+
+KOT: Kotex pads 10s
+
+LIFBY: Lifebouy
+
+LUX: Lux Soap
+
+OHSOH: Oh So Heavenly Gel
+
+PANT: Kotex Pant Liners
+
+PDS: Pads Mix
+
+PERI: Period pants
+
+PROT: Protex Soap
+
+PRVT: Private Joy Tissue
+
+ROM: Romance Soap 50g
+
+SAT: Tissue Single
+
+SATI: Satiskin 2L
+
+SATSK: Satiskin 1L
+
+SON: Sona
+
+TISS: Tissue 4s
+
+TWIN: TwinSaver Tissue
+
+TWINS: TwinSaver 9s
+
+YBO: Yebo Soap
+`;
